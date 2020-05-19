73bce7e724ab158fb3f30f98778b5ff44d7f4819
Hussein Talal <hossientalal2@gmail.com>
2020-05-19 11:34:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-
+  document.getElementById('create-task-form').addEventListener("submit", (e) => {
+    // your code here
+    e.preventDefault();
+  });
 });
 
-document.getElementById('create-task-form').addEventListener("submit", (e) => {
-  // your code here
-  e.preventDefault();
-});
+
